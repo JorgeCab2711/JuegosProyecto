51e6eddb3f700ddcbf0b956da5a7aead2c6c781c
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-03-12 14:41:27 -0600

    Linterna

---
+++ ProyectoFinal/Assets/Scripts/Linterna.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linterna : MonoBehaviour
+{
+
+
+    public AudioClip Linternas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("mouse 1"))
+        {
+            GetComponent<AudioSource>().PlayOneShot(Linternas);
+            GetComponent<Light>().enabled = !GetComponent<Light>().enabled;
+        }
+            
+            
+
+
+
+    }
+}
